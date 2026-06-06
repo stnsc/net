aa591d276583453a4a6cbaedc8085d12e04fb856
stnsc <stanescuvladutgeorge@gmail.com>
2026-06-07 01:59:29 +0300

    Initial commit: Work in Progress page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Work in Progress</h1>
+      <p>Something great is coming soon. Stay tuned.</p>
+    </div>
+  )
+}
+
+export default App
