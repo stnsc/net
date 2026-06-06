b2003a91a86d183be11561f5fda86edf005d14ef
stnsc <stanescuvladutgeorge@gmail.com>
2026-06-07 02:25:46 +0300

    Switched WIP page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
-
 function App() {
   return (
-    <div className="container">
-      <h1>In Development</h1>
-      <p>This page is currently under active development.</p>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}>
+      <img src="/wip.png" alt="Work in progress!" style={{
+        
+      }}/>
     </div>
   )
 }
